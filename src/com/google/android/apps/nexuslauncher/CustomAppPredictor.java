LawnchairLauncher/lawnchair

Enable predictions by default, to match the toggle

src/com/google/android/apps/nexuslauncher/CustomAppPredictor.java
@@ -181,7 +181,7 @@ public class CustomAppPredictor extends UserEventDispatcher implements SharedPre
     }
 
     private boolean isPredictorEnabled() {
-        return Utilities.getPrefs(mContext).getBoolean(SettingsActivity.SHOW_PREDICTIONS_PREF, false);
+        return Utilities.getPrefs(mContext).getBoolean(SettingsActivity.SHOW_PREDICTIONS_PREF, true);
     }
 
     @Override

ABC-KANG/abc_packages_apps_Launcher3

src/com/google/android/apps/nexuslauncher/CustomAppPredictor.java
@@ -177,7 +177,7 @@ public class CustomAppPredictor extends UserEventDispatcher implements SharedPre
     }
 
     private boolean isPredictorEnabled() {
-        return Utilities.getPrefs(mContext).getBoolean(AppDrawerActivity.SHOW_PREDICTIONS_PREF, false);
+        return Utilities.getPrefs(mContext).getBoolean(AppDrawerActivity.SHOW_PREDICTIONS_PREF, true);
     }
 
     @Override